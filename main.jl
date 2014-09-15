9285cde152ee5ad94bdbc6f4ff13620d231fc201
Guillaume <guillaume.viejo@gmail.com>
2014-09-15 19:18:38 +0200

    still shit

--- main.jl
+++ main.jl
@@ -1,4 +1,12 @@
 
+libSDLmm = dlopen("/usr/lib/libSDLmm-0.1.so.8.0.0")
+
+# ccall( dlsym(libsdlmm, :Init), Bool, (Uint32,), 0x00000020);
+
+import GetC.@getCFun
+@getCFun libSDLmm Init Init(flags::Uint32)::Bool
+
+
 # module SDL
 #     import GetC.@getCFun
 #     include("deps.jl")
@@ -52,93 +60,95 @@
 
 # using StrPack
 
-type sdl_color
-  r::Uint8;
-  g::Uint8;
-  b::Uint8;
-  unused::Uint8;
-end
+# type sdl_color
+#   r::Uint8;
+#   g::Uint8;
+#   b::Uint8;
+#   unused::Uint8;
+# end
 
-type sdl_palette
-  ncolors::Int32
-  colors::Ptr{sdl_color}
-end
+# type sdl_palette
+#   ncolors::Int32
+#   colors::Ptr{sdl_color}
+# end
 
-type sdl_pixelformat
-  palette::Ptr{sdl_palette};
-  BitsPerPixel::Uint8; BytesPerPixel::Uint8; 
-  Rloss::Uint8; Gloss::Uint8; Bloss::Uint8; Aloss::Uint8;
-  Rshift::Uint8; Gshift::Uint8; Bshift::Uint8; Ashift::Uint8;
-  Rmask::Uint32; Gmask::Uint32; Bmask::Uint32; Amask::Uint32;
-  colorkey::Uint32;
-  alpha::Uint8;
-end
+# type sdl_pixelformat
+#   palette::Ptr{sdl_palette};
+#   BitsPerPixel::Uint8; BytesPerPixel::Uint8; 
+#   Rloss::Uint8; Gloss::Uint8; Bloss::Uint8; Aloss::Uint8;
+#   Rshift::Uint8; Gshift::Uint8; Bshift::Uint8; Ashift::Uint8;
+#   Rmask::Uint32; Gmask::Uint32; Bmask::Uint32; Amask::Uint32;
+#   colorkey::Uint32;
+#   alpha::Uint8;
+# end
 
-type sdl_rect
-  x::Int16;
-  y::Int16;
-  w::Uint16;
-  h::Uint16;
-end
+# type sdl_rect
+#   x::Int16;
+#   y::Int16;
+#   w::Uint16;
+#   h::Uint16;
+# end
 
-type sdl_surface
-    flags::Uint32;
-    format::Ptr{sdl_pixelformat};
-    w::Int32;
-    h::Int32;
-    pitch::Uint16;
-    pixels::Ptr{Void};
-    clip_rect::sdl_rect;
-    refcount::Int32;
-end
+# type sdl_surface
+#     flags::Uint32;
+#     format::Ptr{sdl_pixelformat};
+#     w::Int32;
+#     h::Int32;
+#     pitch::Uint16;
+#     pixels::Ptr{Void};
+#     clip_rect::sdl_rect;
+#     refcount::Int32;
+# end
 
 
-function sdl_init(flags::Uint32)
-  tmp = ccall( (:SDL_Init, "libSDL"), Int32, (Uint32,), flags);
-end
+# function sdl_init(flags::Uint32)
+#   tmp = ccall( (:SDL_Init, "libSDL"), Int32, (Uint32,), flags);
+# end
 
-function sdl_setvideomode(w, h)
-  return ccall( (:SDL_SetVideoMode, "libSDL"), Ptr{sdl_surface}, (Int32, Int32, Int32, Uint32,), w, h, 32, 0x00000000);
-end
+# function sdl_setvideomode(w, h)
+#   return ccall( (:SDL_SetVideoMode, "libSDL"), Ptr{sdl_surface}, (Int32, Int32, Int32, Uint32,), w, h, 32, 0x00000000);
+# end
 
-function sdl_creatergbsurface(w, h)
-  return ccall( (:SDL_CreateRGBSurface, "libSDL"), Ptr{sdl_surface}, (Uint32, Int32, Int32, Int32, Uint32, Uint32, Uint32, Uint32,),
-                0x00000000, w, h, 32, 0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff);
-end
+# function sdl_creatergbsurface(w, h)
+#   return ccall( (:SDL_CreateRGBSurface, "libSDL"), Ptr{sdl_surface}, (Uint32, Int32, Int32, Int32, Uint32, Uint32, Uint32, Uint32,),
+#                 0x00000000, w, h, 32, 0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff);
+# end
 
-function sdl_blitsurface(src, dst)
-  return ccall( (:SDL_BlitSurface, "libSDL"), Int32, (Ptr{sdl_surface}, Ptr{sdl_rect}, Ptr{sdl_surface}, Ptr{sdl_rect}), &src, &C_NULL, &dst, &C_NULL);
-end
+# function sdl_blitsurface(src, dst)
+#   return ccall( (:SDL_BlitSurface, "libSDL"), Int32, (Ptr{sdl_surface}, Ptr{sdl_rect}, Ptr{sdl_surface}, Ptr{sdl_rect}), &src, &C_NULL, &dst, &C_NULL);
+# end
 
-function blit_map(map, screen, w, h)
-  for i = 0:w
-    for j = 0:h
-      color = sdl_mapRGB()
-      put_pixel(map, i,j,color)
-    end
-  end
-  # sdl_blitsurface(map, screen)
-  # SDL_UpdateRect(screen, 0, 0, width, height)
-end
+# function blit_map(map, screen, w, h)
+#   for i = 0:w
+#     for j = 0:h
+#       color = sdl_mapRGB()
+#       put_pixel(map, i,j,color)
+#     end
+#   end
+#   # sdl_blitsurface(map, screen)
+#   # SDL_UpdateRect(screen, 0, 0, width, height)
+# end
 
-function put_pixel(surface, x, y, color)
-  tmp = unsafe
-  bufp::Ptr{Uint32} = surface
-end
-width = 640
-height = 480
+# function put_pixel(surface, x, y, color)
+#   tmp = unsafe
+#   bufp::Ptr{Uint32} = surface
+# end
+# width = 640
+# height = 480
 
-sdl_init(0x00000020)
-screen = sdl_setvideomode(width, height)
-map = sdl_creatergbsurface(width, height)
 
-put_pixel()
 
-# sdl_blitsurface(map, screen)
+# sdl_init(0x00000020)
+# screen = sdl_setvideomode(width, height)
+# map = sdl_creatergbsurface(width, height)
 
-# function put_pixel(x,y,r,g,b)
-#   color = SDL_MapRGB(map.format)
+# put_pixel()
+
+# # sdl_blitsurface(map, screen)
+
+# # function put_pixel(x,y,r,g,b)
+# #   color = SDL_MapRGB(map.format)
   
 
-# end
+# # end
 
