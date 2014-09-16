1efafa15f3d2b977d792dfb9457d2034f3ebf7a6
Guillaume <guillaume.viejo@gmail.com>
2014-09-16 18:40:57 +0200

    at last

--- yourlib.c
+++ yourlib.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
 #include "yourlib.h"
 
 int init(Uint32 flags)
@@ -7,12 +8,15 @@ int init(Uint32 flags)
 	int res = SDL_Init(flags);
 	return res;
 };
+void quit(Uint32 flags) {
+	SDL_QuitSubSystem(flags);
+	SDL_Quit();	
+};
 struct SDL_Surface* setvideomode(int w, int h, int bpp, Uint32 flags) {
     SDL_Surface *tmp = SDL_SetVideoMode(w, h, bpp, flags);    
 	return tmp;
 };
-struct SDL_Surface* creatergbsurface(int w, int h)
-{	
+struct SDL_Surface* creatergbsurface(int w, int h) {
 	SDL_Surface *surface;
 	Uint32 rmask, gmask, bmask, amask;
 	#if SDL_BYTEORDER == SDL_BIG_ENDIAN
@@ -29,19 +33,42 @@ struct SDL_Surface* creatergbsurface(int w, int h)
 	surface = SDL_CreateRGBSurface(0, w, h, 32, rmask, gmask, bmask, amask);
 	return surface;
 };
-void putpixel(struct SDL_Surface* map, int x, int y, Uint8 r, Uint8 g, Uint8 b)
-{	
-	SDL_LockSurface(map);
-	Uint32 color = SDL_MapRGB(map->format, r, g, b);
-	Uint32 *bufp = *( ( Uint32 * )map->pixels + y * map->w + x );
-	*bufp = color;
-	SDL_UnlockSurface(map);
-
-
+void setpixel(struct SDL_Surface* surface, int x, int y, Uint8 r, Uint8 g, Uint8 b) {		
+	*((Uint32*)(surface->pixels) + x + y * surface->w) = SDL_MapRGB(surface->format, r, g, b);;		
 };
-
-void update(struct SDL_Surface* src, struct SDL_Surface* dest, int w, int h)
-{
+void setfullcolor(struct SDL_Surface* surface, Uint8 r, Uint8 g, Uint8 b) {
+    Uint32 *pixels = (Uint32 *) surface->pixels;
+	SDL_LockSurface(surface);    
+    int i;
+    for (i = 0; i < surface->w*surface->h; i++)
+    {
+    	pixels[i] = SDL_MapRGB(surface->format, r, g, b);    
+    }
+    SDL_UnlockSurface(surface);
+};
+void update(struct SDL_Surface* src, struct SDL_Surface* dest, int w, int h) {
 	SDL_BlitSurface(src, NULL, dest, NULL);	
 	SDL_UpdateRect(dest, 0, 0, w, h);
-}
+};
+void background(struct SDL_Surface* surface, char* image_path) {	
+   SDL_Surface *image = IMG_Load (image_path);
+   if ( !image )
+   {
+      printf ( "IMG_Load: %s\n", IMG_GetError () );  
+   }   
+   SDL_BlitSurface(image, NULL, surface, 0);
+   SDL_UpdateRect(surface, 0, 0, surface->w, surface->h);
+   SDL_FreeSurface(image);   
+};
+void sdl_events() {	
+	printf("Hello");
+	SDL_Event event;
+    // if( SDL_PollEvent( &event ) ) 
+    // {
+    // 	if( event.type == SDL_QUIT )
+    // 		return 0;
+    // 	else
+    // 		return 1;
+    // }	
+	// return 1;
+};
--- yourlib.h
+++ yourlib.h
@@ -1,5 +1,5 @@
 /**
-gcc -fPIC yourlib.c -shared -o libtest.so `sdl-config --cflags --libs`
+gcc -fPIC yourlib.c -shared -o libtest.so `sdl-config --cflags --libs` -lSDL_image
 */
 
 extern int init(Uint32 flags);
@@ -8,8 +8,11 @@ extern struct SDL_Surface* setvideomode(int w,
 										int bpp, 
 										Uint32 flags);
 extern struct SDL_Surface* creatergbsurface(int w, int h);
-extern void putpixel(struct SDL_Surface* map, 
+extern void setpixel(struct SDL_Surface* surface, 
 									int x, 
 									int y, 
 									Uint8 r, Uint8 g, Uint8 b);
+extern void setfullcolor(struct SDL_Surface* surface, Uint8 r, Uint8 g, Uint8 b);
 extern void update(struct SDL_Surface* src, struct SDL_Surface* dest, int w, int h);
+extern void background(struct SDL_Surface *surface, char *image_path);
+extern void sdl_events(void);
